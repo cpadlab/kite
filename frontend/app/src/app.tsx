9818cb0adc4c151c1e0c2d1bad41b46f2067fcfe
Carlos Padilla <105047274+cpadlab@users.noreply.github.com>
2026-08-17 20:17:35 +0200

    .

--- frontend/app/src/app.tsx
+++ frontend/app/src/app.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { ThemeProvider } from './context/theme.tsx'
 import { AuthProvider } from './context/auth.tsx'
@@ -11,6 +11,8 @@ import LoadingScreen from './components/blocks/loading/component.tsx'
 
 const LoginPage = lazy(() => import('./pages/public/login/page.tsx'))
 const PublicLayout = lazy(() => import('./pages/public/layout.tsx'))
+const TOTPPage = lazy(() => import('./pages/public/totp/page.tsx'))
+const DashboardPage = lazy(() => import('./pages/private/home/dashboard/page.tsx'))
 
 function App() {
     return (
@@ -25,13 +27,15 @@ function App() {
                             
                             <Route element={<PublicLayout />}>
                                 <Route path="/login" element={<LoginPage />} />
+                                <Route path="/login/totp" element={<TOTPPage />} />
                             </Route>
 
                             <Route element={<Layout />}>
-                                
-                          
-                                
+                                <Route path="/" element={<DashboardPage />} />
                             </Route>
+
+                            <Route path="/404" element={<div className="flex min-h-screen items-center justify-center font-semibold text-lg text-muted-foreground bg-background">404 - Page Not Found</div>} />
+                            <Route path="*" element={<Navigate to="/404" replace />} />
                             
                         </Routes>
                     </Suspense>
--- frontend/app/src/layout/layout.tsx
+++ frontend/app/src/layout/layout.tsx
@@ -1,9 +1,22 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '@/context/auth'
 import { SidebarProvider } from "@/components/ui/sidebar"
 
 const Layout = () => {
+    
+    const { isAuthenticated, isLoading } = useAuth()
+
+    if (isLoading) {
+        return null
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />
+    }
+
     return (
         <SidebarProvider>
-        
+            <Outlet />
         </SidebarProvider>
     )
 }
